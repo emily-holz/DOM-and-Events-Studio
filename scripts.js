acb89cede1f7d0311c08c13b4775d670805f165a
emily-holz <emily.r.holzman@gmail.com>
2023-02-10 20:33:26 -0500

    fixed final errors

--- scripts.js
+++ scripts.js
@@ -1,13 +1,14 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
 
-window.addEventListener('load', function() {
+window.addEventListener("load", function() {
 
-    let takeOffBtn = getElementById("takeoff");
-    takeOffBtn.addEventListener('click', function() {
+    console.log("loaded")
+
+    let takeOffBtn = document.getElementById("takeoff");
+    takeOffBtn.addEventListener("click", function() {
         //when the take off button is clicked ...
         let isReadyToLaunch = window.confirm("Confirm that the shuttle is ready for takeoff.")
-        console.log(isReadyToLaunch);
         if(isReadyToLaunch) {
             let flightStatus = document.getElementById("flightStatus");
             flightStatus.innerHTML = "Shuttle in flight.";
@@ -18,8 +19,8 @@ window.addEventListener('load', function() {
         }
     });
 
-    let landingBtn = getElementById("landing");
-    landingBtn.addEventListener('click', function() {
+    let landingBtn = document.getElementById("landing");
+    landingBtn.addEventListener("click", function() {
         window.alert("The shuttle is landing. Landing gear engaged.");
         let flightStatus = document.getElementById("flightStatus");
         flightStatus.innerHTML = "The shuttle has landed.";
@@ -30,20 +31,20 @@ window.addEventListener('load', function() {
     });
 
     let abortMissionBtn = document.getElementById("missionAbort");
-    abortMissionBtn.addEventListener('click', function() {
+    abortMissionBtn.addEventListener("click", function() {
         let shouldAbortMission = window.confirm("Confirm that you want to abort the mission.");
         if (shouldAbortMission) {
             let flightStatus = document.getElementById("flightStatus");
             flightStatus.innerHTML = "Mission aborted.";
             let shuttleFlightScreen = document.getElementById("shuttleBackground");
-            shuttleFlightScreen.setAttribute("style", "background-color: blue");
+            shuttleFlightScreen.setAttribute("style", "background-color: green");
             let shuttleHeight = document.getElementById("spaceSHuttleHeight");
-            shuttleHeight.innerText = "10000";
+            shuttleHeight.innerText = "0";
         }
     });
 
     let upBtn = document.getElementById("up");
-    upBtn.addEventListener('click', function(){
+    upBtn.addEventListener("click", function(){
         let shuttleHeight = document.getElementById("spaceSHuttleHeight");
         let shuttleHeightAmount = Number.shuttleHeight.innerText
         shuttleHeightAmount += 10000;
@@ -51,7 +52,7 @@ window.addEventListener('load', function() {
     });
 
     let downBtn = document.getElementById("down");
-    downBtn.addEventListener('click', function(){
+    downBtn.addEventListener("click", function(){
         let shuttleHeight = document.getElementById("spaceSHuttleHeight");
         let shuttleHeightAmount = Number.shuttleHeight.innerText
         shuttleHeightAmount -= 10000;
